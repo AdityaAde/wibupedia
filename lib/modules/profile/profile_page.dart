4fc5f1a7143574e7c9bb9978755a6f394c66f45e
Aditya <adidtyaad@gmail.com>
2023-11-30 10:19:03 +0700

    fix: routing authentication when success signin

--- lib/modules/profile/profile_page.dart
+++ lib/modules/profile/profile_page.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wibupedia/modules/modules.dart';
 
 @RoutePage()
 class ProfilePage extends StatelessWidget {
@@ -7,6 +9,13 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Search')));
+    return Scaffold(
+      body: Center(
+        child: InkWell(
+          child: const Text('Logout'),
+          onTap: () => context.read<AuthenticationCubit>().logout(),
+        ),
+      ),
+    );
   }
 }
