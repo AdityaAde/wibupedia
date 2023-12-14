f117b895b48a432149610c505c7c41182d59655d
Aditya Ade <adidtyaad@gmail.com>
2023-12-15 00:34:48 +0700

    feat: add detail anime after search

--- lib/modules/search/widgets/list_search_anime_widget.dart
+++ lib/modules/search/widgets/list_search_anime_widget.dart
@@ -1,6 +1,8 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../component/route/routers.gr.dart';
 import '../../../component/theme/theme.dart';
 import '../../../widgets/widgets.dart';
 import '../controller/controller.dart';
@@ -22,32 +24,40 @@ class ListSearchAnimeWidget extends StatelessWidget {
           )),
           success: (anime) => SliverList(
             delegate: SliverChildBuilderDelegate(
-              (context, index) => Container(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                height: 111,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: SizedBox(
-                        width: 100,
-                        height: 100,
-                        child: ImageCachedWidget(
-                          url: anime.search?[index].thumb ?? '',
+              (context, index) => InkWell(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  height: 111,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: ImageCachedWidget(
+                            url: anime.search?[index].thumb ?? '',
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(width: 20),
-                    Expanded(
-                      flex: 2,
-                      child: Text(
-                        anime.search?[index].title ?? '',
-                        style: AppStyle.materialTextStyle.titleMedium
-                            ?.copyWith(fontWeight: FontWeight.w700),
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    )
-                  ],
+                      const SizedBox(width: 20),
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          anime.search?[index].title ?? '',
+                          style: AppStyle.materialTextStyle.titleMedium
+                              ?.copyWith(fontWeight: FontWeight.w700),
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                onTap: () => context.pushRoute(
+                  DetailAnimeRoute(
+                    titleAnime: anime.search?[index].endpointAnime ?? '',
+                    animeUrl: anime.search?[index].endpoint ?? '',
+                  ),
                 ),
               ),
               childCount: anime.search?.length,
